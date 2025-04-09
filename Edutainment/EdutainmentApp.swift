1535cd2db74602572c7ab07e8b8fbc822806f3a0
Ali Syed <alisyed0206@gmail.com>
2025-04-08 23:18:09 -0400

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Ali Syed on 2025-04-08.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EdutainmentTests/EdutainmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentTests.swift
+//  EdutainmentTests
+//
+//  Created by Ali Syed on 2025-04-08.
+//
+
+import Testing
+@testable import Edutainment
+
+struct EdutainmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
